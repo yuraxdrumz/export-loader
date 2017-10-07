1d9f910db09e31ee770ea0d82a366c9f7fd9f456
yurik1776 <yurik1776@gmail.com>
2017-10-07 16:06:04 +0300

    added readme

--- test-utils/resourcePath.js
+++ test-utils/resourcePath.js
@@ -2,6 +2,7 @@
 const Utilizer = function(parm1, parm2){
   console.log(parm1, parm2)
 }
+
 export default function(){
   console.log('dsadsasdasd')
 }
@@ -13,9 +14,3 @@ export function Public(){
 let B = '2'
 
 const D = '2'
-
-export {
-  Utilizer,
-  B,
-  D
-}
